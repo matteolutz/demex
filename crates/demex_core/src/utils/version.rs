5f378206e77d6bb0e88fd722b0dde5651f455f8b
Matteo Lutz <matteo.lutz06@outlook.de>
2026-02-04 17:35:37 +0100

    fix: use lazy static for demex version

--- crates/demex_core/src/utils/version.rs
+++ crates/demex_core/src/utils/version.rs
@@ -1,9 +1,17 @@
+use lazy_static::lazy_static;
+
 pub const VERSION_STR: &str = env!("CARGO_PKG_VERSION");
 
+lazy_static! {
+    static ref DEMEX_VERSION: (u8, u8, u8) = {
+        let version = VERSION_STR.split('.').collect::<Vec<&str>>();
+        let major = version[0].parse::<u8>().unwrap();
+        let minor = version[1].parse::<u8>().unwrap();
+        let patch = version[2].parse::<u8>().unwrap();
+        (major, minor, patch)
+    };
+}
+
 pub fn demex_version() -> (u8, u8, u8) {
-    let version = VERSION_STR.split('.').collect::<Vec<&str>>();
-    let major = version[0].parse::<u8>().unwrap();
-    let minor = version[1].parse::<u8>().unwrap();
-    let patch = version[2].parse::<u8>().unwrap();
-    (major, minor, patch)
+    *DEMEX_VERSION
 }
